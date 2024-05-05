9327d2ae5862d636f497012231706c0e941aa035
Vedant <rajputvedant213@gmail.com>
2024-05-05 11:47:24 +0530

    sign up page

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterLinkActive,],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
